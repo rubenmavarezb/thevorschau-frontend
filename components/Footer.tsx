445a88400d97d4129ffad6c5a89b911a0f6b6810
Ruben Mavarez <rmavarez@glamit.com.ar>
2021-02-06 15:15:51 -0300

    teaching ruben how to use next

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function Footer() {
     return (
       <footer>
-        <Link href="/login">LOGIN</Link>
-        <Link href="/new-account">New account</Link>
+        <Link href="/login">LOGIN de footer</Link>
+        <Link href="/new-account">New account footer</Link>
       </footer>
     )
 }
