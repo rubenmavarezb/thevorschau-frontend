81030d686f847efccd29446fdf054c87584cf02c
angelicamolero <angelicamoleror8@gmail.com>
2021-02-14 22:00:05 -0300

    modifying navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,20 @@
 import Link from 'next/link';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUser, faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
     return (
-      <nav>
-        <Link href="/login">LOGIN</Link>
-        <Link href="/new-account">New account</Link>
-      </nav>
+      <div className="navbar">
+          <div className="burgermenu">
+            <i className="fas fa-bars"></i>
+          </div>
+          <a href="">VORSHAU</a>
+          <nav>
+            <FontAwesomeIcon icon={faSearch} size="2x"/>
+            <Link href="/login"><FontAwesomeIcon icon={faUser} size="2x"/></Link>
+            <Link href="/login"><FontAwesomeIcon icon={faHeart} size="2x"/></Link>
+            <Link href="/login"><FontAwesomeIcon icon={faShoppingCart} size="2x"/></Link>
+          </nav>
+      </div>
     )
 }
