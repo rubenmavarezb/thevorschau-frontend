fc61ad201e33d58b2fbbf3c8480d70b2515bdb9e
Ruben Mavarez <rmavarez@glamit.com.ar>
2021-02-15 13:53:39 -0300

    test

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,10 +11,10 @@ export default function Layout({children}: Children) {
       <>
         <Head>
           <title>The Vorschau | Clothes for everyone </title>
-          {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossOrigin="anonymous" /> */}
         </Head>
+
         <Navbar/>
-        {children}
+          {children}
         <Footer/>
       </>
     )
