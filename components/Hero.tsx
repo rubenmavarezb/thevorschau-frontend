d2a15ad7a3a3513418e2305761b59e58e1de56f3
angelicamolero <angelicamoleror8@gmail.com>
2021-02-17 17:11:38 -0300

    creating hero base

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,18 @@
 export default function Hero() {
     return (
         <main>
-            <h1>Hero</h1>
+            <div className="hero">
+                <div className="hero__container">
+                    <div className="hero__container__text">
+                        <h1>The voushar</h1>
+                         <p>lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum</p>
+                         <button>shop now</button>
+                    </div>
+                    <div className="hero__container__img">
+                        <img src="" alt=""/>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
