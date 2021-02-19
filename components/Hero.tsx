bf3a275a1486ac9f28c8fa51a5b18a670da0af26
angelicamolero <angelicamoleror8@gmail.com>
2021-02-18 21:22:21 -0300

    fixing hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,12 +5,12 @@ export default function Hero() {
         <main>
             <div className="hero">
                 <div className="hero__container">
-                    <div className="hero__container__text">
+                    <div className="hero__container-text">
                         <h1>The vorschau</h1>
                          <p>lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum</p>
                          <button className="btn btn-hero">shop now</button>
                     </div>
-                    <div className="hero__container__img">
+                    <div className="hero__container-img">
                         <img src="/hero-guy.png" alt=""/>
                     </div>
                 </div>
